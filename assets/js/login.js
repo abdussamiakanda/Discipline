5b10c0a3a3330ae40afe420a9069649f8bc773ea
Md. Abdus Sami Akanda <abdussamiakanda@gmail.com>
2021-08-15 22:31:06 +0600

    profile is done

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,6 +5,7 @@ var userdata = null;
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const page = urlParams.get('page');
+const pageid = urlParams.get('id');
 
 document.getElementById('login_btn').addEventListener('click', GoogleLogin)
 
@@ -48,24 +49,10 @@ function verifyUser(user){
   })
 }
 
-function registerUser(user){
-  document.title = "Sign Up";
-  alertMessage(type="success", "Your email is verified by your CR!<br>Now it's time to sign up.")
-  document.getElementById('login_form').style.display="none";
-  document.getElementById('verify_id').style.display="none";
-  document.getElementById('signup_form').style.display="block";
-}
-
-function verified(user){
-  document.title = "Dashboard";
-  document.getElementById('login_form').style.display="none";
-  document.getElementById('signup_form').style.display="none";
-  showDashboard(user);
-}
-
 document.getElementById("signup_btn").onclick = function (){
   var name = document.getElementById('name').value;
   var id = document.getElementById('id').value;
+  var batch = document.getElementById('batch').value;
   var year = document.getElementById('year').value;
   var term = document.getElementById('term').value;
   var contact = document.getElementById('contact').value;
@@ -74,11 +61,12 @@ document.getElementById("signup_btn").onclick = function (){
   database.ref('/users/'+userdata.uid).update({
     name: name,
     id: id,
+    batch: batch,
     year: year,
     term: term,
     contact: contact,
     blood: blood_group,
-    type: general
+    type: 'general'
   })
   database.ref('/verified-users/'+id).update({
     name: name
@@ -94,44 +82,12 @@ function GoogleLogout() {
     document.getElementById('verify_id').style.display="none";
     document.getElementById('signup_form').style.display="none";
     document.getElementById('dashboard_container').style.display = "none";
+    document.getElementById('header_right').innerHTML = "";
+    document.getElementById('fa_bars').innerHTML = "";
+    window.setTimeout(function(){ window.location.assign("./"); }, 1500);
   }).catch((e)=>{
     console.log(e)
   })
-}
-
-function showDashboard(user){
-  document.getElementById('dashboard_container').style.display = "flex";
-  document.getElementById('fa_bars').innerHTML = `<i class="search-icon fa fa-bars" onclick="hideshowDashMenu()" aria-hidden="true"></i>`;
-  showHeader(user);
-  checkPage(user);
-}
-
-function showHeader(user){
-  document.getElementById('header_right').innerHTML = `
-    <a href="./search.html"><i class="search-icon fa fa-search" aria-hidden="true"></i></a>
-    <div>${user.displayName}</div>
-    <div class="dropdown">
-      <div class="user-image-div"><img class="header-image" src="${user.photoURL}" alt=""></div>
-      <div class="dropdown-menu">
-        <a href="./profile.html"><div class="dropdown-menu-item">Profile</div></a>
-        <div class="dropdown-menu-item" onClick="GoogleLogout()" style="cursor:pointer;">Logout</div>
-      </div>
-    </div>`
-}
-
-function hideshowDashMenu(){
-  var x = document.getElementById('dashboard_left');
-  if(x.style.display === "none"){
-    x.style.display = "flex";
-  } else {
-    x.style.display = "none";
-    document.getElementById('dashboard_right').style.width = "100%";
-  }
-}
-
-function changeURL(id){
-  window.location.search = "page="+id;
-  return false;
 }
 
 function checkPage(user){
@@ -141,6 +97,10 @@ function checkPage(user){
     showAcademicCalendar(user);
   } else if(page === 'term-courses'){
     showTermCourses(user);
+  } else if(page === 'course'){
+    showCourseDetail(user);
+  } else if(page === 'profile'){
+    showProfile(user);
   } else if(page === 'my-courses'){
     // showMyCourses(user);
   } else if(page === 'attendance'){
@@ -153,63 +113,116 @@ function checkPage(user){
 }
 
 function showUserDashboard(user){
+  document.getElementById('header_middle').innerHTML = `<h4>Dashboard</h4>`;
   document.getElementById('dashboard').classList.remove('hide');
 }
 
 function showAcademicCalendar(user){
+  document.getElementById('header_middle').innerHTML = `<h4>Academic Calendar</h4>`;
   document.getElementById('academic-calendar').classList.remove('hide');
 }
 
 function showTermCourses(user){
   document.getElementById('term-courses').classList.remove('hide');
+  database.ref('/users/'+user.uid).once("value").then((snapshot) => {
+    var term = snapshot.child('term').val();
+    document.getElementById('header_middle').innerHTML = `<h4>${term} Term Courses</h4>`;
+
+    database.ref('/'+term+'-term/courses').orderByKey().once("value").then((snapshot) => {
+      snapshot.forEach(function(childSnapshot){
+        database.ref('/'+term+'-term/courses/'+childSnapshot.key).once("value").then((snapshot) => {
+          var no = snapshot.child('no').val().toUpperCase();
+          var title = snapshot.child('title').val();
+          var type = snapshot.child('type').val();
+          var secA = snapshot.child('secA/teacher').val();
+          var secB = snapshot.child('secB/teacher').val();
+
+          var courseDiv = document.getElementById('term-courses');
+          var courseEl = `
+            <div class="course-item" onclick="changeURL('course','${no}')">
+              <h6>${no} (${type})</h6>
+              <h4>${title}</h4> <hr>
+              Section A: ${secA} <br>
+              Section B: ${secB}
+            </div>
+          `
+          courseDiv.innerHTML += courseEl;
+        })
+      })
+    })
+  })
 }
-// document.getElementById('header_middle').innerHTML = `<h4>Dashboard</h4>`;
+
+function showCourseDetail(user){
+  document.getElementById('header_middle').innerHTML = `<h4>Course No: ${pageid}</h4>`;
+  document.getElementById('single-course').classList.remove('hide');
+}
+
+function showProfile(user){
+  document.getElementById('header_middle').innerHTML = `<h4>Profile</h4>`;
+  document.getElementById('profile-page').classList.remove('hide');
+  document.getElementById('dashboard_left').style.display = "none"
+  document.getElementById('dashboard_right').style.width = "100%";
+
+  document.getElementById('hello-user').innerHTML = `
+    <img src="${user.photoURL}" alt="">
+    <h4>Welcome, ${user.displayName}</h4>
+  `;
+  database.ref('/users/'+user.uid).once("value").then((snapshot) => {
+    var name = snapshot.child('name').val();
+    var id = snapshot.child('id').val();
+    var batch = snapshot.child('batch').val();
+    var year = snapshot.child('year').val();
+    var term = snapshot.child('term').val();
+    var contact = snapshot.child('contact').val();
+    var blood = snapshot.child('blood').val();
+
+    document.getElementById('user-data').innerHTML = `
+      <table>
+        <tr><td>Name</td><td> : </td><td>${name}</td></tr>
+        <tr><td>Student ID</td><td> : </td><td>${id}</td></tr>
+        <tr><td>Batch</td><td> : </td><td>${batch}</td></tr>
+        <tr><td>Year</td><td> : </td><td>${year}</td></tr>
+        <tr><td>Term</td><td> : </td><td>${term}</td></tr>
+        <tr><td>Contact</td><td> : </td><td>${contact}</td></tr>
+        <tr><td>Blood Group</td><td> : </td><td>${blood}</td></tr>
+      </table>
+      <button class="signup-btn" onClick="hideEditProfile()">Edit Profile</button>
+    `;
+  })
+}
+
+function saveUserData(){
+  var name = document.getElementById('name1').value;
+  var id = document.getElementById('id1').value;
+  var batch = document.getElementById('batch1').value;
+  var year = document.getElementById('year1').value;
+  var term = document.getElementById('term1').value;
+  var contact = document.getElementById('contact1').value;
+  var blood_group = document.getElementById('blood_group1').value;
+
+  database.ref('/users/'+userdata.uid).update({
+    name: name,
+    id: id,
+    batch: batch,
+    year: year,
+    term: term,
+    contact: contact,
+    blood: blood_group
+  })
+  document.getElementById('user-data').classList.remove('hide');
+  document.getElementById('profile_form').classList.add('hide');
+  showProfile(userdata);
+  alertMessage(type="success", 'Your profile is updated!')
+}
+
+function hideEditProfile(){
+  showEditProfileData(userdata,database);
+  document.getElementById('user-data').classList.add('hide');
+  document.getElementById('profile_form').classList.remove('hide');
+}
 
 
-//
-// function FriendsHandler(userdata){
-//   database.ref('/'+userdata.uid+'/profile').once("value").then((snapshot) => {
-//     var public = snapshot.child("public").val();
-//     if(public === true){
-//       document.getElementById("friends").innerHTML = `
-//         <div class="dropdown-menu-item">Friends</div>
-//       `
-//       document.getElementById("friends-icon").innerHTML = `
-//         <i class="search-icon fa fa-users" aria-hidden="true"></i>
-//       `
-//       document.getElementById("message-icon").innerHTML = `
-//         <i class="search-icon fa fa-comments-o" aria-hidden="true"></i>
-//       `
-//     }else{
-//     }
-//   })
-// }
-//
 
 
 checkAuthState()
-//
-// function showReqFriendsNum(user){
-//   database.ref('/'+user.uid+'/friends').orderByKey().once("value").then((snapshot) => {
-//     var num = 0;
-//     snapshot.forEach(function(childSnapshot){
-//       var profileId = childSnapshot.key;
-//       database.ref('/'+user.uid+'/friends/'+profileId).once("value").then((snapshot) => {
-//         var status = snapshot.child('status').val();
-//         if (status === false){
-//           num++;
-//           showNumber(num);
-//         }else{
-//           num = num;
-//         }
-//       });
-//     });
-//   });
-// }
-//
-// function showNumber(num){
-//   document.getElementById('friends-icon').style.position = 'relative';
-//   document.getElementById('friends-icon').innerHTML += `
-//     <i class="badge-dot fa fa-circle" aria-hidden="true"></i>
-//   `
-// }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,192 @@
+function registerUser(user){
+  document.title = "Sign Up";
+  alertMessage(type="success", "Your email is verified by your CR!<br>Now it's time to sign up.")
+  document.getElementById('login_form').style.display="none";
+  document.getElementById('verify_id').style.display="none";
+  document.getElementById('signup_form').style.display="block";
+}
+
+function verified(user){
+  document.title = "Dashboard";
+  document.getElementById('login_form').style.display="none";
+  document.getElementById('signup_form').style.display="none";
+  showDashboard(user);
+}
+
+function showDashboard(user){
+  document.getElementById('dashboard_container').style.display = "flex";
+  document.getElementById('fa_bars').innerHTML = `<i class="search-icon fa fa-bars" onclick="hideshowDashMenu()" aria-hidden="true"></i>`;
+  showHeader(user);
+  checkPage(user);
+}
+
+function showHeader(user){
+  document.getElementById('header_right').innerHTML = `
+    <a href="./search.html"><i class="search-icon fa fa-search" aria-hidden="true"></i></a>
+    <div>${user.displayName}</div>
+    <div class="dropdown">
+      <div class="user-image-div" onclick="hideshowDropMenu()"><img class="header-image" src="${user.photoURL}" alt=""></div>
+      <div class="dropdown-menu" id="dropdown-menu">
+        <div class="dropdown-menu-item" onclick="changeURL('profile',null)">Profile</div>
+        <div class="dropdown-menu-item" onClick="GoogleLogout()">Logout</div>
+      </div>
+    </div>`
+}
+
+function hideshowDashMenu(){
+  var x = document.getElementById('dashboard_left');
+  if(x.style.display === "none"){
+    x.style.display = "flex";
+    document.getElementById('dashboard_right').style.width = "80%";
+  } else {
+    x.style.display = "none";
+    document.getElementById('dashboard_right').style.width = "100%";
+  }
+}
+
+function hideshowDropMenu(){
+  if(document.getElementById('dropdown-menu').style.display === "none"){
+    document.getElementById('dropdown-menu').style.display = "block";
+  } else {
+    document.getElementById('dropdown-menu').style.display = "none";
+  }
+}
+
+function changeURL(page,id){
+  if(id){
+    window.location.search = "page="+page+"&id="+id;
+  }else{
+    window.location.search = "page="+page;
+  }
+  return false;
+}
+
+function cancelEditProfile(){
+  document.getElementById('user-data').classList.remove('hide');
+  document.getElementById('profile_form').classList.add('hide');
+}
+
+function showEditProfileData(user,database){
+  database.ref('/users/'+user.uid).once("value").then((snapshot) => {
+    var name = snapshot.child('name').val();
+    var id = snapshot.child('id').val();
+    var batch = snapshot.child('batch').val();
+    var year = snapshot.child('year').val();
+    var term = snapshot.child('term').val();
+    var contact = snapshot.child('contact').val();
+    var blood = snapshot.child('blood').val();
+    var years = ['1st','2nd','3rd','4th'];
+    var terms = ['11','12','21','22','31','32','41','42'];
+    var bloods = ['A (+ve)','B (+ve)','AB (+ve)','O (+ve)','A (-ve)','B (-ve)','AB (-ve)','O (-ve)'];
+    var isYear = [];
+    var isTerm = [];
+    var isBlood = [];
+
+    for (let x=0; x < years.length; x++){
+      if (year === years[x]){
+        isYear[x] = "selected";
+        console.log(x,isYear[x])
+      }else{
+        isYear[x] = null;
+      }
+    }
+    for (let y=0; y < terms.length; y++){
+      if (term === terms[y]){
+        isTerm[y] = "selected";
+        console.log(y,isTerm[y])
+      }else{
+        isTerm[y] = null;
+      }
+    }
+    for (let z=0; z < bloods.length; z++){
+      if (blood === bloods[z]){
+        isBlood[z] = "selected";
+        console.log(z,isBlood[z])
+      }else{
+        isBlood[z] = null;
+      }
+    }
+
+    document.getElementById('profile_form').innerHTML = `
+    <form onSubmit="return false;">
+      <table>
+        <tr>
+          <td>Name</td><td> : </td>
+          <td><input type="text" id="name1" placeholder="Enter your name.." value="${name}"></td>
+        </tr>
+        <tr>
+          <td>Student ID</td><td> : </td>
+          <td><input type="number" id="id1" placeholder="Enter student ID.." pattern="/^-?\d+\.?\d*$/" onKeyPress="if(this.value.length==6) return false;" value="${id}"></td>
+        </tr>
+        <tr>
+          <td>Batch</td><td> : </td>
+          <td><input type="number" id="batch1" placeholder="Enter your batch.." pattern="/^-?\d+\.?\d*$/" onKeyPress="if(this.value.length==2) return false;" value="${batch}"></td>
+        </tr>
+        <tr>
+          <td>Year</td><td> : </td>
+          <td>
+            <select id="year1">
+              <option value="1st" ${isYear[0]}>1st Year</option>
+              <option value="2nd" ${isYear[1]}>2nd Year</option>
+              <option value="3rd" ${isYear[2]}>3rd Year</option>
+              <option value="4th" ${isYear[3]}>4th Year</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>Term</td><td> : </td>
+          <td>
+            <select id="term1">
+              <option value="11" ${isTerm[0]}>11 Term</option>
+              <option value="12" ${isTerm[1]}>12 Term</option>
+              <option value="21" ${isTerm[2]}>21 Term</option>
+              <option value="22" ${isTerm[3]}>22 Term</option>
+              <option value="31" ${isTerm[4]}>31 Term</option>
+              <option value="32" ${isTerm[5]}>32 Term</option>
+              <option value="41" ${isTerm[6]}>41 Term</option>
+              <option value="42" ${isTerm[7]}>42 Term</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>Contact</td><td> : </td>
+          <td><input type="number" id="contact1" placeholder="Enter contact number.." pattern="/^-?\d+\.?\d*$/" onKeyPress="if(this.value.length==11) return false;" value="${contact}"></td>
+        </tr>
+        <tr>
+          <td>Blood Group</td><td> : </td>
+          <td>
+            <select id="blood_group1">
+              <option value="A (+ve)" ${isBlood[0]}>A (+ve)</option>
+              <option value="B (+ve)" ${isBlood[1]}>B (+ve)</option>
+              <option value="AB (+ve)" ${isBlood[2]}>AB (+ve)</option>
+              <option value="O (+ve)" ${isBlood[3]}>O (+ve)</option>
+              <option value="A (-ve)" ${isBlood[4]}>A (-ve)</option>
+              <option value="B (-ve)" ${isBlood[5]}>B (-ve)</option>
+              <option value="AB (-ve)" ${isBlood[6]}>AB (-ve)</option>
+              <option value="O (-ve)" ${isBlood[7]}>O (-ve)</option>
+            </select>
+          </td>
+        </tr>
+      </table>
+      <button type="submit" class="reverse-btn" onClick="saveUserData()">Save Profile</button>
+    </form>
+    <button class="signup-btn" onClick="cancelEditProfile()">Cancel</button>
+    `
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function alertMessage(type="success", message){
     let x = document.getElementById("alerts")
